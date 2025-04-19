e92117d397bba93531c240667716ca7279d9c24c
deepanshu <21f3001217@ds.study.iitm.ac.in>
2025-04-19 21:17:58 +0530

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/logo.png';
+import bell from '../assets/bell.png';
+import users from '../assets/users.png';
 
-const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
+interface HeaderProps {
+  title: string;
 }
 
-export default Header
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <header className="w-full h-16 bg-[#151515] flex items-center justify-between px-[88px] shadow-sm">
+      <div className="flex items-center space-x-3">
+        <img
+          src={logo}
+          alt="Logo"
+          className="w-[213px] h-[39.09px] object-contain"
+        />
+      </div>
+
+      <div className="flex items-center w-[97.11px] h-[32px] gap-4">
+        <img
+          src={bell}
+          alt="Notifications"
+          className="w-[29.11px] h-[27.89px] object-contain"
+        />
+        <img
+          src={users}
+          alt="User"
+          className="w-[52px] h-[32px] object-contain"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
